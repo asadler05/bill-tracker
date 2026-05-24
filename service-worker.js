a09260fe8aef056ad13a51d1bcf693ef374397cb
asadler05 <alex.sadler@gmail.com>
2026-05-24 15:55:04 -0400

    updated version for last change

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ------------------------------
 // VERSION — bump this on each deploy
 // ------------------------------
-const SW_VERSION = "v1.0.4";
+const SW_VERSION = "v1.0.5";
 
 // ------------------------------
 // Notify clients that a new SW is installed
